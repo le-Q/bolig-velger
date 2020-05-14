6b7fd22e07c95fc6bc3bb2eb474029f3a91c1884
Simen <lsimen89@gmail.com>
2020-05-14 13:44:59 +0200

    Changed layout

--- public/views/shortcode_template.php
+++ public/views/shortcode_template.php
@@ -2,209 +2,213 @@
 
 // No hotspots are defined
 $has_hotspots = false;
-if ( ! empty( $settings['hotspots']['0'] ) ) {	
+if (!empty($settings['hotspots']['0'])) {
 	foreach ($settings['hotspots'] as $key => $hotspot) {
-		if ( ! empty( $hotspot['coordinates'] ) ) {
+		if (!empty($hotspot['coordinates'])) {
 			$has_hotspots = true;
 			break;
 		}
 	}
 }
 
-if ( empty( $has_hotspots ) ) : ?>
-	<p><?php _e( 'You need to define some clickable areas for your image.', 'bolig-velger' ); ?></p>
-	<p><?php echo edit_post_link( __( 'Edit Image', 'bolig-velger' ), false, false, $settings['image_id'] ); ?></p>
+if (empty($has_hotspots)) : ?>
+	<p><?php _e('You need to define some clickable areas for your image.', 'bolig-velger'); ?></p>
+	<p><?php echo edit_post_link(__('Edit Image', 'bolig-velger'), false, false, $settings['image_id']); ?></p>
 
-<?php // In page builder edit mode - just display the image ?>
-<?php elseif ( !empty( $_GET['fl_builder'] ) || !empty( $_GET['elementor-preview'] ) || ( !empty( $_GET['action'] ) && $_GET['action'] == 'elementor' ) ): ?>
+	<?php // In page builder edit mode - just display the image 
+	?>
+<?php elseif (!empty($_GET['fl_builder']) || !empty($_GET['elementor-preview']) || (!empty($_GET['action']) && $_GET['action'] == 'elementor')) : ?>
 	<div class="hotspots-image-container">
-		<img
-			width="<?php echo $settings['img_width']; ?>"
-			height="<?php echo $settings['img_height']; ?>"
-			src="<?php echo $settings['img_url']; ?>"
-			alt="<?php echo esc_attr( $settings['img_alt'] ); ?>"
-			class="hotspots-image skip-lazy"
-			data-id="<?php echo $settings['image_id']; ?>"
-			data-no-lazy="1"
-			data-lazy-src=""
-			data-lazy="false"
-			>
+		<img width="<?php echo $settings['img_width']; ?>" height="<?php echo $settings['img_height']; ?>" src="<?php echo $settings['img_url']; ?>" alt="<?php echo esc_attr($settings['img_alt']); ?>" class="hotspots-image skip-lazy" data-id="<?php echo $settings['image_id']; ?>" data-no-lazy="1" data-lazy-src="" data-lazy="false">
 	</div>
-<?php // There are hotspots! Show the interactive image ?>
+	<?php // There are hotspots! Show the interactive image 
+	?>
 <?php else : ?>
 
-<style>
-	#<?php echo $settings['spot_id']; ?> .hotspots-image-container,
-	#<?php echo $settings['spot_id']; ?> .leaflet-container {
-		background: <?php echo $settings['img_bg']; ?>
-	}
+	<style>
+		#<?php echo $settings['spot_id']; ?>.hotspots-image-container,
+		#<?php echo $settings['spot_id']; ?>.leaflet-container {
+			background: <?php echo $settings['img_bg']; ?>
+		}
 
-	#<?php echo $settings['spot_id']; ?> .hotspots-placeholder {
-		background: <?php echo $settings['more_info_bg']; ?>;
-		border: 0 <?php echo $settings['more_info_bg']; ?> solid;
-		color: <?php echo $settings['more_info_text']; ?>;
-	}
+		#<?php echo $settings['spot_id']; ?>.hotspots-placeholder {
+			background: <?php echo $settings['more_info_bg']; ?>;
+			border: 0 <?php echo $settings['more_info_bg']; ?> solid;
+			color: <?php echo $settings['more_info_text']; ?>;
+		}
 
-	#<?php echo $settings['spot_id']; ?> .hotspot-title {
-		color: <?php echo $settings['more_info_title']; ?>;
-	}
+		#<?php echo $settings['spot_id']; ?>.hotspot-title {
+			color: <?php echo $settings['more_info_title']; ?>;
+		}
 
-	<?php foreach ($formatted_styles as $style) : ?>
-		#<?php echo $settings['spot_id']; ?> .hotspot-<?php echo $style['name']; ?> {
+		<?php foreach ($formatted_styles as $style) : ?>#<?php echo $settings['spot_id']; ?>.hotspot-<?php echo $style['name']; ?> {
 			stroke-width: <?php echo $style['borderWidth']; ?>;
 			fill: <?php echo $style['display']['fillColor']; ?>;
 			fill-opacity: <?php echo $style['display']['fillOpacity']; ?>;
 			stroke: <?php echo $style['display']['borderColor']; ?>;
 			stroke-opacity: <?php echo $style['display']['borderOpacity']; ?>;
 		}
-		#<?php echo $settings['spot_id']; ?> .hotspot-<?php echo $style['name']; ?>:hover,
-		#<?php echo $settings['spot_id']; ?> .hotspot-<?php echo $style['name']; ?>.hotspot-active {
-			fill: <?php echo (esc_html(get_post_meta( $hotspot['action'], '_cmb2_leilighet_status', true)) == 'ledig') ? "#F00" : '#0F0'; ?>;
+
+		#<?php echo $settings['spot_id']; ?>.hotspot-<?php echo $style['name']; ?>:hover,
+		#<?php echo $settings['spot_id']; ?>.hotspot-<?php echo $style['name']; ?>.hotspot-active {
+			fill: <?php echo (esc_html(get_post_meta($hotspot['action'], '_cmb2_leilighet_status', true)) == 'ledig') ? "#F00" : '#0F0'; ?>;
 			fill-opacity: <?php echo "0.7" ?>;
 			stroke: <?php echo $style['hover']['borderColor']; ?>;
 			stroke-opacity: <?php echo $style['hover']['borderOpacity']; ?>;
 		}
-	<?php endforeach; ?>
-	#<?php echo $settings['spot_id']; ?> .leaflet-tooltip,
-	#<?php echo $settings['spot_id']; ?> .leaflet-rrose-content-wrapper {
-		background: <?php echo $settings['more_info_bg']; ?>;
-		border-color: <?php echo $settings['more_info_bg']; ?>;
-		color: <?php echo $settings['more_info_text']; ?>;
-	}
 
-	#<?php echo $settings['spot_id']; ?> a.leaflet-rrose-close-button {
-		color: <?php echo $settings['more_info_title']; ?>;
-	}
+		<?php endforeach; ?>#<?php echo $settings['spot_id']; ?>.leaflet-tooltip,
+		#<?php echo $settings['spot_id']; ?>.leaflet-rrose-content-wrapper {
+			background: <?php echo $settings['more_info_bg']; ?>;
+			border-color: <?php echo $settings['more_info_bg']; ?>;
+			color: <?php echo $settings['more_info_text']; ?>;
+		}
 
-	#<?php echo $settings['spot_id']; ?> .leaflet-rrose-tip {
-		background: <?php echo $settings['more_info_bg']; ?>;
-	}
+		#<?php echo $settings['spot_id']; ?>a.leaflet-rrose-close-button {
+			color: <?php echo $settings['more_info_title']; ?>;
+		}
 
-	#<?php echo $settings['spot_id']; ?> .leaflet-popup-scrolled {
-		border-bottom-color: <?php echo $settings['more_info_text']; ?>;
-		border-top-color: <?php echo $settings['more_info_text']; ?>;
-	}
+		#<?php echo $settings['spot_id']; ?>.leaflet-rrose-tip {
+			background: <?php echo $settings['more_info_bg']; ?>;
+		}
 
-	#<?php echo $settings['spot_id']; ?> .leaflet-tooltip-top:before {
-		border-top-color: <?php echo $settings['more_info_bg']; ?>;
-	}
+		#<?php echo $settings['spot_id']; ?>.leaflet-popup-scrolled {
+			border-bottom-color: <?php echo $settings['more_info_text']; ?>;
+			border-top-color: <?php echo $settings['more_info_text']; ?>;
+		}
 
-	#<?php echo $settings['spot_id']; ?> .leaflet-tooltip-bottom:before {
-		border-bottom-color: <?php echo $settings['more_info_bg']; ?>;
-	}
-	#<?php echo $settings['spot_id']; ?> .leaflet-tooltip-left:before {
-		border-left-color: <?php echo $settings['more_info_bg']; ?>;
-	}
-	#<?php echo $settings['spot_id']; ?> .leaflet-tooltip-right:before {
-		border-right-color: <?php echo $settings['more_info_bg']; ?>;
-	}
-</style>
+		#<?php echo $settings['spot_id']; ?>.leaflet-tooltip-top:before {
+			border-top-color: <?php echo $settings['more_info_bg']; ?>;
+		}
 
-<?php /*
+		#<?php echo $settings['spot_id']; ?>.leaflet-tooltip-bottom:before {
+			border-bottom-color: <?php echo $settings['more_info_bg']; ?>;
+		}
+
+		#<?php echo $settings['spot_id']; ?>.leaflet-tooltip-left:before {
+			border-left-color: <?php echo $settings['more_info_bg']; ?>;
+		}
+
+		#<?php echo $settings['spot_id']; ?>.leaflet-tooltip-right:before {
+			border-right-color: <?php echo $settings['more_info_bg']; ?>;
+		}
+	</style>
+
+	<?php /*
 <script>
 	window.daStyles<?php echo $settings['image_id']; ?> = <?php echo json_encode($formatted_styles); ?>
 </script>
 */ ?>
 
 	<div class="hotspots-container <?php echo $settings['urls_class']; ?> layout-<?php echo $settings['layout']; ?> event-<?php echo $settings['event_trigger']; ?>" id="<?php echo $settings['spot_id']; ?>" data-layout="<?php echo $settings['layout']; ?>" data-trigger="<?php echo $settings['event_trigger']; ?>">
-		<div class="hotspots-interaction">
-			<?php if ( $settings['urls_only'] ) {
-				require( $this->get_plugin_dir() . '/public/views/image_template.php' );
-			} else  {
-				require( $this->get_plugin_dir() . '/public/views/more_info_template.php' );
-				require( $this->get_plugin_dir() . '/public/views/image_template.php' );
+		<!--<div class="hotspots-container">-->
+		<div class="bolig-velger-area">
+			<?php if ($settings['urls_only']) {
+				require($this->get_plugin_dir() . '/public/views/image_template.php');
+			} else {
+				require($this->get_plugin_dir() . '/public/views/image_template.php');
+				require($this->get_plugin_dir() . '/public/views/more_info_template.php');
 			} ?>
 		</div>
 		<map name="hotspots-image-<?php echo $settings['image_id']; ?>" class="hotspots-map">
-			<?php foreach( $settings['hotspots'] as $key => $hotspot ) : ?>
+			<?php foreach ($settings['hotspots'] as $key => $hotspot) : ?>
 				<?php
-					$coords = $hotspot['coordinates'];
-					$target = !empty( $hotspot['action'] ) ? $hotspot['action'] : '';
-					$new_window = !empty( $hotspot['action-url-open-in-window'] ) ? $hotspot['action-url-open-in-window'] : '';
-					$target_window = $new_window == 'on' ? '_new' : '';
-					$target_url = !empty( $hotspot['action-url-url'] ) ? $hotspot['action-url-url'] : '';
-					$area_class = $target == 'url' ? 'url-area' : 'more-info-area';
-					$href = $target == 'url' ? $target_url : '#hotspot-' . $settings['spot_id'] . '-' . $key;
-					$href = !empty($href) ? $href : '#';
-					$title = !empty( $hotspot['action'] ) ? $hotspot['action'] : '';
-					if ( empty( $hotspot['description'] ) ) {
-						$hotspot['description'] = '';
-					}
-					if ( empty( $settings['img_settings']['_bv_has_multiple_styles']['0'] ) || $settings['img_settings']['_bv_has_multiple_styles']['0'] != 'on' || empty( $hotspot['style'] ) ) {
-						$color_scheme = '';
-					} else {
-						$color_scheme = $hotspot['style'];
-					}
+				$coords = $hotspot['coordinates'];
+				$target = !empty($hotspot['action']) ? $hotspot['action'] : '';
+				$new_window = !empty($hotspot['action-url-open-in-window']) ? $hotspot['action-url-open-in-window'] : '';
+				$target_window = $new_window == 'on' ? '_new' : '';
+				$target_url = !empty($hotspot['action-url-url']) ? $hotspot['action-url-url'] : '';
+				$area_class = $target == 'url' ? 'url-area' : 'more-info-area';
+				$href = $target == 'url' ? $target_url : '#hotspot-' . $settings['spot_id'] . '-' . $key;
+				$href = !empty($href) ? $href : '#';
+				$title = !empty($hotspot['action']) ? $hotspot['action'] : '';
+				if (empty($hotspot['description'])) {
+					$hotspot['description'] = '';
+				}
+				if (empty($settings['img_settings']['_bv_has_multiple_styles']['0']) || $settings['img_settings']['_bv_has_multiple_styles']['0'] != 'on' || empty($hotspot['style'])) {
+					$color_scheme = '';
+				} else {
+					$color_scheme = $hotspot['style'];
+				}
 
 				?>
-				<area
-					shape="poly"
-					coords="<?php echo $coords; ?>"
-					href="<?php echo $href; ?>"
-					title="<?php echo $title; ?>"
-					alt="<?php echo esc_attr( $title ); ?>"
-					data-action="<?php echo $target; ?>"
-					data-color-scheme="<?php echo $color_scheme; ?>"
-					target="<?php echo $target_window; ?>"
-					class="<?php echo $area_class; ?>"
-					>
+				<area shape="poly" coords="<?php echo $coords; ?>" href="<?php echo $href; ?>" title="<?php echo $title; ?>" alt="<?php echo esc_attr($title); ?>" data-action="<?php echo $target; ?>" data-color-scheme="<?php echo $color_scheme; ?>" target="<?php echo $target_window; ?>" class="<?php echo $area_class; ?>">
 			<?php endforeach; ?>
 		</map>
 
 		<?php /* Error message for admins when there's a JS error */
-		if ( ! empty( $_GET['bv_debug'] ) ) : ?>
+		if (!empty($_GET['bv_debug'])) : ?>
 			<div id="error-<?php echo $settings['spot_id']; ?>" class="da-error">
 				<p>It looks like there is a JavaScript error in a plugin or theme that is causing a conflict with Draw Attention. For more information on troubleshooting this issue, please see our <a href="https://wpdrawattention.com/document/troubleshooting-conflicts-themes-plugins/" target="_new">help page</a>.
 			</div>
 		<?php endif; ?>
 
 		<?php /* Loop through the hotspots and output the more info content for each */
-		foreach( $settings['hotspots'] as $key => $hotspot ) : ?>
-			<?php if ( empty( $settings['img_settings']['_bv_has_multiple_styles']['0'] ) || $settings['img_settings']['_bv_has_multiple_styles']['0'] != 'on' || empty( $hotspot['style'] ) ) {
+		foreach ($settings['hotspots'] as $key => $hotspot) : ?>
+			<?php if (empty($settings['img_settings']['_bv_has_multiple_styles']['0']) || $settings['img_settings']['_bv_has_multiple_styles']['0'] != 'on' || empty($hotspot['style'])) {
 				$color_scheme_class = '';
 			} else {
 				$color_scheme_class = 'bv-style-' . $hotspot['style'];
 			}
-			
-			if ( empty( $hotspot['title'] ) ) {
+
+			if (empty($hotspot['title'])) {
 				$hotspot['title'] = '';
 			}
 
 			?>
 			<div class="hotspot-info <?php echo $color_scheme_class; ?>" id="hotspot-<?php echo $settings['spot_id']; ?>-<?php echo $key; ?>">
 				<?php
-				if ( !empty( $hotspot['action'] ) ) {
-					if ( 'bigcommerce' === $hotspot['action'] ) {
-						echo DrawAttention_BigCommerce_Action::render_hotspot_content( $hotspot, $settings );
+				if (!empty($hotspot['action'])) {
+					if ('bigcommerce' === $hotspot['action']) {
+						echo DrawAttention_BigCommerce_Action::render_hotspot_content($hotspot, $settings);
 						echo '</div>';
 						continue;
 					}
 				}
 				?>
 
-				<?php echo apply_filters( 'boligvelger_hotspot_title', '<h2 class="hotspot-title"> Leilighet ' . get_the_title($hotspot['action']) . '</h2>', $hotspot ); ?>
-				<?php if ( !empty($hotspot['detail_image_id'])) : ?>
+				<?php echo apply_filters('boligvelger_hotspot_title', '<h2 class="bolig-velger-title"> Leilighet ' . get_the_title($hotspot['action']) . '</h2>', $hotspot); ?>
+				<?php if (!empty($hotspot['detail_image_id'])) : ?>
 					<div class="hotspot-thumb">
-						<?php echo wp_get_attachment_image( $hotspot['detail_image_id'], apply_filters( 'bv_detail_image_size', 'large', $hotspot, $settings['img_post'], $settings['img_settings'] ) ); ?>
+						<?php echo wp_get_attachment_image($hotspot['detail_image_id'], apply_filters('bv_detail_image_size', 'large', $hotspot, $settings['img_post'], $settings['img_settings'])); ?>
 					</div>
-				<?php elseif( empty( $hotspot['detail_image_id'] ) && ! empty( $hotspot[ 'detail_image' ] ) ) : ?>
+				<?php elseif (empty($hotspot['detail_image_id']) && !empty($hotspot['detail_image'])) : ?>
 					<div class="hotspot-thumb">
 						<img src="<?php echo $hotspot['detail_image']; ?>">
 					</div>
 				<?php endif; ?>
 				<div class="hotspot-content">
-					<?php if( !empty( $hotspot['description'] ) ) echo apply_filters( 'bv_description', do_shortcode( $wp_embed->autoembed( $wp_embed->run_shortcode( $hotspot['description'] ) ) ) ); ?>
-				 <?php echo get_post_field('post_content', $hotspot['action']) ?><!-- kode her for å fylle inn info om leiligheten! --->
 
-				 <?php
-					// Grab the metadata from the database
-					$text = get_post_meta( $hotspot['action'], '_cmb2_leilighet_status', true );
+					<div class="aprt-content">
+						<?php
+						$image = get_the_post_thumbnail($hotspot['action']); ?>
 
-					// Echo the metadata
-					echo esc_html( $text );
-					echo $hotspot['action'];
-				?>
-
+						<div class="aprt-image">
+							<?php echo $image; ?>
+						</div>
+						<div class="aprt-content-text">
+							<?php
+							if (!empty($hotspot['description'])) echo apply_filters('bv_description', do_shortcode($wp_embed->autoembed($wp_embed->run_shortcode($hotspot['description'])))); ?>
+							<?php echo get_post_field('post_content', $hotspot['action']) ?>
+							<!-- kode her for å fylle inn info om leiligheten! --->
+							<?php
+							// Grab the metadata from the database
+							$status = get_post_meta($hotspot['action'], '_cmb2_leilighet_status', true);
+							$nr = get_post_meta($hotspot['action'], '_cmb2_leilighet_nr', true);
+							$floor = get_post_meta($hotspot['action'], '_cmb2_leilighet_etasje', true);
+							$area = get_post_meta($hotspot['action'], '_cmb2_leilighet_bruttoareal', true);
+							$rooms = get_post_meta($hotspot['action'], '_cmb2_leilighet_antall', true);
+							$price = get_post_meta($hotspot['action'], '_cmb2_leilighet_pris', true);
+							?>
+						</div>
+					</div>
+					<div class="aprt-content-info">
+						<span class="status">Status: <?php echo $status ?></span>
+						<span class="nr">Leilighet: <?php echo $nr ?></span>
+						<span class="floor">Etasje: <?php echo $floor ?></span>
+						<span class="rooms">Antall rom: <?php echo $rooms ?></span>
+						<span class="price">Pris: <?php echo $price ?></span>
+					</div>
 				</div>
 			</div>
 		<?php endforeach; ?>
