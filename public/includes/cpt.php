e5781959e48f8de7fd88772b06dc60bd43af531f
le-Q <quang.d.le@hiof.no>
2020-05-19 13:20:12 +0200

    bolig velger -> boligvelger

--- public/includes/cpt.php
+++ public/includes/cpt.php
@@ -25,16 +25,16 @@ class DrawAttention_CPT {
 		$result = register_post_type( $this->post_type, /* (http://codex.wordpress.org/Function_Reference/register_post_type) */
 		 	// let's now add all the options for this post type
 			array('labels' => array(
-				'name' => __('Bolig Velger', $this->post_type.' general name', 'bolig-velger' ), /* This is the Title of the Group */
-				'singular_name' => __('Bilde', $this->post_type.' singular name', 'bolig-velger' ), /* This is the individual type */
-				'all_items' => __('Alle Bilder', 'bolig-velger' ), /* the all items menu item */
+				'name' => __('Boligvelger', $this->post_type.' general name', 'bolig-velger' ), /* This is the Title of the Group */
+				'singular_name' => __('Blokk', $this->post_type.' singular name', 'bolig-velger' ), /* This is the individual type */
+				'all_items' => __('Alle Blokk', 'bolig-velger' ), /* the all items menu item */
 				'add_new' => __('Legg til ny', 'custom '.$this->post_type.' item', 'bolig-velger' ), /* The add new menu item */
-				'add_new_item' => __('Legg til bilde', 'bolig-velger' ), /* Add New Display Title */
+				'add_new_item' => __('Legg til blokk', 'bolig-velger' ), /* Add New Display Title */
 				'edit' => __( 'Edit' ), /* Edit Dialog */
-				'edit_item' => __('Endre bilde', 'bolig-velger' ), /* Edit Display Title */
-				'new_item' => __('Nytt bilde', 'bolig-velger' ), /* New Display Title */
-				'view_item' => __('Vis bilde', 'bolig-velger' ), /* View Display Title */
-				'search_items' => __('Søkte bilder', 'bolig-velger' ), /* Search CPT_SINGULAR_NAME Title */
+				'edit_item' => __('Endre blokk', 'bolig-velger' ), /* Edit Display Title */
+				'new_item' => __('Nytt blokk', 'bolig-velger' ), /* New Display Title */
+				'view_item' => __('Vis blokk', 'bolig-velger' ), /* View Display Title */
+				'search_items' => __('Søkte blokk', 'bolig-velger' ), /* Search CPT_SINGULAR_NAME Title */
 				'not_found' =>  __('Nothing found in the Database.', 'bolig-velger' ), /* This displays if there are no entries yet */
 				'not_found_in_trash' => __('Nothing found in Trash', 'bolig-velger' ), /* This displays if there is nothing in the trash */
 				'parent_item_colon' => ''
